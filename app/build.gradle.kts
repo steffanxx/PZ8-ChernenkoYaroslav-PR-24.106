4be42f6fa13954e0ce3405e7f309d20b166cf760
steffann.xx <steffann.xx@outlook.com>
2025-12-05 10:06:45 +0700

    alpha 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
 
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.wear:wear:1.3.0")
 
     implementation("androidx.wear:wear:1.3.0")
     implementation("androidx.wear.widget:wear:1.2.0")
--- app/src/main/java/com/example/pz8_chernenko_yaroslav_pr24106/MainActivity.java
+++
@@ -1,4 +0,0 @@
-package com.example.pz8_chernenko_yaroslav_pr24106;
-
-public class MainActivity {
-}
